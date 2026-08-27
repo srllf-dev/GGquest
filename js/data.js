dc3eff2066f72ada434ed0145c90aea513a3d70a
srllf-code <srllf@cesar.school>
2026-08-26 23:01:49 -0300

    att: atualização js data.js

--- js/data.js
+++ js/data.js
@@ -1,4 +1,37 @@
-// data.js
-// Responsável por ler e escrever no localStorage.
-// Ainda vazio — a lógica de persistência entra aqui quando começarmos
-// a implementar a Biblioteca.
+export const jogos = [
+  {nome: "Minecraft",
+  capa: "",
+  genero: "Sandbox",
+  nota: 9.5,
+  status: "jogando",
+  favorito: true,
+  tags: ["Nostalgia"],
+  review: "muito bom, mas poderia ter mais atualizações",
+  dataInicio: null,
+  dataConclusao: null,
+  completoCem: false},
+
+  {nome: "The Witcher 3",
+  capa: "",
+  genero: "RPG",
+  nota: 8,
+  status: "concluido",
+  favorito: false,
+  tags: ["Aventura", "Fantasia"],
+  review: "um dos melhores jogos que já joguei, história incrível e gráficos impressionantes",
+  dataInicio: null,
+  dataConclusao: null,
+  completoCem: true},
+
+  {nome: "Red Dead Redemption 2",
+  capa: "",
+  genero: "Ação",
+  nota: 10,
+  status: "jogando",
+  favorito: false,
+  tags: ["Western", "Aventura"],
+  review: "um jogo incrível com uma história envolvente e gráficos impressionantes",
+  dataInicio: null,
+  dataConclusao: null,
+  completoCem: false}
+];
--- js/main.js
+++ js/main.js
@@ -4,3 +4,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("GGquest carregado.");
 });
+
+
+import { jogos } from "./data.js";
+
+jogos.forEach(jogo => {
+    console.log(jogo.nome);
+    console.log(jogo.genero);
+    console.log(jogo.nota);
+});
+
+let html  = '';
+
+jogos.forEach(jogo => {
+  html += `<p>${jogo.nome} - ${jogo.genero} - Nota: ${jogo.nota}</p>`;
+})
+
+document.querySelector("jogos-lista").innerHTML = html;
